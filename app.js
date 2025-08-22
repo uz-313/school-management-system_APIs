2aa897706b8760f2a721327f459ae5d7310d3254
Umair Kasam <umairksocial313512@gmail.com>
2025-08-22 21:14:37 +0530

    Update app.js file for production

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const schoolsRoutes = require('./routes/schoolRoute.js');
 const db = require('./config/db.js');
 
 const app = express();
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
